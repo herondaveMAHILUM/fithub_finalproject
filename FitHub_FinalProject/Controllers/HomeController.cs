348329f864e9a05cc98ebabe6435f47719901bb2
Heron Dave <edu.herond04@gmail.com>
2026-04-23 08:29:00 +0800

    First Commit

---
+++ FitHub_FinalProject/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using FitHub_FinalProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FitHub_FinalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
