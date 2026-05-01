446203ee6019d089b9e16c3ad6e003171f956d67
Heron Dave <edu.herond04@gmail.com>
2026-05-01 17:18:12 +0800

    Added AccountController

---
+++ FitHub_FinalProject/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitHub_FinalProject.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
