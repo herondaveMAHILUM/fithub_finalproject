a863f4d3d157a8a6365a14f7fabc50e22de2ecc5
Heron Dave <edu.herond04@gmail.com>
2026-05-01 17:33:19 +0800

    Added Controller for Register Page

--- FitHub_FinalProject/Controllers/AccountController.cs
+++ FitHub_FinalProject/Controllers/AccountController.cs
@@ -8,5 +8,10 @@ namespace FitHub_FinalProject.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
