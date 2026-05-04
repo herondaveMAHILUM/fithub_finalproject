90303c47d10d8766cef2debfa1ad85252f9faa9b
Heron Dave <edu.herond04@gmail.com>
2026-05-04 17:51:05 +0800

    Added Controller for User Dashboard

---
+++ FitHub_FinalProject/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FitHub_FinalProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
