20c820e485f2aa36410b1647bce44dd6dd8bef22
Earl <you@example.com>
2026-05-06 09:05:10 +0800

    Added live stats to home page

--- FitHub_FinalProject/Controllers/HomeController.cs
+++ FitHub_FinalProject/Controllers/HomeController.cs
@@ -1,32 +1,42 @@
+using System.Diagnostics;
+using FitHub_FinalProject.Data;
 using FitHub_FinalProject.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
 namespace FitHub_FinalProject.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly FitHubDbContext _context;
+
+        public HomeController(FitHubDbContext context)
         {
-            ViewBag.TotalMembers = 0;
-            ViewBag.TotalEquipmentTypes = 0;
-            ViewBag.DaysOpen = 0;
-            ViewBag.TotalTrainers = 0;
+            _context = context;
+        }
 
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.TotalMembers = await _context.Users.CountAsync(u => u.IsActive);
+            ViewBag.TotalTrainers = await _context.Trainers.CountAsync(t => t.IsActive);
+            ViewBag.TotalEquipmentTypes = 20;
+            ViewBag.DaysOpen = 365;
             ViewBag.Testimonials = null;
-
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Privacy() => View();
+
+        public IActionResult Terms() => View("Privacy");
+
+        public IActionResult Contact()
         {
-            return View();
+            TempData["InfoMessage"] = "Contact us at support@fithub.ph";
+            return RedirectToAction("Index");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        public IActionResult Error() =>
+            View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
